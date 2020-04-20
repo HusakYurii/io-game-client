85121d598856e3d4f52719ed4d51e9e7995b197c
Husak Yurii <gusak.1881@gmail.com>
2020-04-21 00:03:16 +0200

    Game and FSM have been initialized to run game's first state (Closes #4)

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const clientSocket = io('http://localhost:9090');
+import { Game, Controller, Model, View } from "./game/index.js"
+import { gameConfig } from "./configs/gameConfig.js";
+
+const game = new Game({}, new Controller(new Model(gameConfig), new View()));
+
+import { StateMachine, GamePausedState, GameOverState, PreloadState, LoginState, GameState } from "./fsm/index.js";
+
+const fsm = new StateMachine(game);
+fsm.registrStates([
+    new PreloadState(fsm),
+    new LoginState(fsm),
+    new GameState(fsm),
+    new GamePausedState(fsm),
+    new GameOverState(fsm)
+]);
+
+game.init();
+
+fsm.changeStateTo("PreloadState");
+
+if(gameConfig.isDebuggerMode) {
+    window.Game = game;
+    window.StateMachine = fsm;
+}
+
