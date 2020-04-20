5c807085e23d4d30a3815eb54b2821a876c7084e
Husak Yurii <gusak.1881@gmail.com>
2020-04-20 23:53:08 +0200

    game config was added with basic options

---
+++ configs/gameConfig.js
@@ -0,0 +1,8 @@
+export const gameConfig = {
+    isDebuggerMode: true,
+    ioUrl: 'http://localhost:9090',
+    assets: [
+        { name: 'icon-1', url: './assets/icon-1.png' },
+        { name: 'icon-2', url: './assets/icon-2.png' }
+    ]
+};
