a95a792a16711397df708b73712538cc08876f8c
Husak Yurii <gusak.1881@gmail.com>
2020-04-20 21:20:10 +0200

    Add index file with export satements (Updates #2)

---
+++ fsm/index.js
@@ -0,0 +1,6 @@
+export * from "./StateMachine.js";
+export * from "./states/GamePausedState.js";
+export * from "./states/GameOverState.js";
+export * from "./states/PreloadState.js";
+export * from "./states/LoginState.js";
+export * from "./states/GameState.js";
