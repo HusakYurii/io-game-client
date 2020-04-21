2f60a8e4d7dff38a901af8c34ed113464741c061
Husak Yurii <gusak.1881@gmail.com>
2020-04-21 22:17:08 +0200

    More dependencies were added for pixi (Closes #5)

--- libs/PixiCustomized.js
+++ libs/PixiCustomized.js
@@ -7,27 +7,34 @@
 // Import Application class that is the main part of our PIXI project
 import { Application } from '@pixi/app';
 
-import { Sprite } from '@pixi/sprite'; // Import Sprite to be used on stage
-import { Container } from '@pixi/display'; // Import Container to be used on stage
+export * from '@pixi/sprite'; // Import Sprite to be used on stage
+export * from '@pixi/display'; // Import Container to be used on stage
+export * from '@pixi/sprite-animated';
+export * from '@pixi/text';
 
 // In order that PIXI could render things we need to register appropriate plugins in Renderer
 import { Renderer } from '@pixi/core';
-
 // BatchRenderer is the "plugin" for drawing sprites
 import { BatchRenderer } from '@pixi/core';
+// To enable sprites be interactive and use interaction API
+import * as interaction from '@pixi/interaction';
+
 Renderer.registerPlugin('batch', BatchRenderer);
+Renderer.registerPlugin('interaction', interaction.InteractionManager);
 
 // TickerPlugin is the plugin for running an update loop (it's for the application class)
 import { TickerPlugin } from '@pixi/ticker';
 Application.registerPlugin(TickerPlugin);
 
-// And just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
+// Register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
 import { AppLoaderPlugin } from '@pixi/loaders';
 Application.registerPlugin(AppLoaderPlugin);
 
-export {
-    Application,
-    Container,
-    Sprite
-}
+// Register SpritesheetLoader on Loader plugins
+import { Loader } from '@pixi/loaders';
+import { SpritesheetLoader } from '@pixi/spritesheet';
+Loader.registerPlugin(SpritesheetLoader);
 
+export {
+    Application
+}
