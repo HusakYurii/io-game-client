69d0f45aedd0eafcbd1b3e89c9e1283ae7604964
Husak Yurii <gusak.1881@gmail.com>
2020-04-21 23:07:11 +0200

    New Layers were used in game (Closes #6)

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-import { Game, Controller, Model, View } from "./game/index.js"
-import { gameConfig } from "./configs/gameConfig.js";
+import { Game, Controller, Model, View } from "./game"
+import { gameConfig } from "./configs";
 
 const game = new Game({}, new Controller(new Model(gameConfig), new View()));
 
-import { StateMachine, GamePausedState, GameOverState, PreloadState, LoginState, GameState } from "./fsm/index.js";
+import { BackgroundLayer, ControlLayer, GameLayer, UILayer } from "./game/layers";
+
+game.setViewLayers([
+    new BackgroundLayer(0),
+    new GameLayer(1),
+    new ControlLayer(2),
+    new UILayer(3)
+]);
+
+import { StateMachine, GamePausedState, GameOverState, PreloadState, LoginState, GameState } from "./fsm";
 
 const fsm = new StateMachine(game);
 fsm.registrStates([
@@ -18,8 +27,7 @@ game.init();
 
 fsm.changeStateTo("PreloadState");
 
-if(gameConfig.isDebuggerMode) {
+if (gameConfig.isDebuggerMode) {
     window.Game = game;
     window.StateMachine = fsm;
 }
-
