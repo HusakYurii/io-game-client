7decff1fce87953b3c83286d8f84e16e76b50672
Husak Yurii <gusak.1881@gmail.com>
2020-04-28 23:27:28 +0200

    Model was updated to store game data (Closes #12)

--- game/Model.js
+++ game/Model.js
@@ -3,5 +3,22 @@ export class Model {
         this.gameConfig = gameConfig;
         this.playerId = "";
         this.roomId = "";
+        this.name = "";
+
+        this.serverUpdates = {};
+    }
+
+    updateUserData({ id, roomId, name } = {}) {
+        this.playerId = id;
+        this.roomId = roomId;
+        this.name = name;
+    }
+
+    setServerUpdates(data) {
+        this.serverUpdates = data;
+    }
+
+    getServerUpdates() {
+        return this.serverUpdates;
     }
 }
